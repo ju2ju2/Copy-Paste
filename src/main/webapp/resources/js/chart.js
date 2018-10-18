2c1064277c7ee656b4c74d86af334d61a47cb2f8
eunajjing <k_eaaaaa@naver.com>
2018-10-18 20:25:41 +0900

    차트 데이터 약간 수정, dataTables.css와 userProfile.png는 뭐하는 애들인지 모르겠지만 일단 같이 커밋

--- src/main/webapp/resources/js/chart.js
+++ src/main/webapp/resources/js/chart.js
@@ -4,6 +4,34 @@ $(function() {
 	var memberxAxis = [];
 	var memberyAxis = [];
 	
+	function subject() {
+		$.ajax({
+			url:"../etc/stateNoteSubject.json",
+			dataType:"json",
+			success:function(responsedata){
+				$.each(responsedata, function(index, obj) {
+					console.log(obj);
+					subjectxAxis[index] = obj.subjectxAxis;
+					subjectyAxis[index] = obj.subjectyAxis;
+				})
+			}
+		});
+		
+		
+		Highcharts.chart('chartDiv', {
+			chart: { type: 'column' },
+			title: { text: '주제 별 노트 개수' },
+			xAxis: { categories: subjectxAxis },
+			yAxis : { title : false, allowDecimals: false },
+			colors: ['#f56a6a'],
+			legend: false,
+			series : [ {
+				name : '등록된 게시글 수',
+				data : subjectyAxis
+			} ]	
+		});
+	}
+	
 		function makeChart() {
 			$.ajax({
 				url:"../etc/stateMember.json",
@@ -16,6 +44,7 @@ $(function() {
 					})
 				}
 			});
+			
 			Highcharts.chart('chartDiv', {
 				title : {
 					text : '달별 가입한 회원 수',
@@ -60,20 +89,7 @@ $(function() {
 		});
 		
 		$('#noteStatistic').click(function() {
-			
-			Highcharts.chart('chartDiv', {
-				chart: { type: 'column' },
-				title: { text: '주제 별 노트 개수' },
-				xAxis: { categories: subjectxAxis },
-				yAxis : { title : false, allowDecimals: false },
-				colors: ['#f56a6a'],
-				legend: false,
-				series : [ {
-					name : '등록된 게시글 수',
-					data : subjectyAxis
-				} ]	
-			});
-			
+			subject();
 		});
 		
 		makeChart();
