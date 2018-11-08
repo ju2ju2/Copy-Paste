a57da480dfd421becca3a91b6f8cb27f78df776c
woonana <naviya0609@gmail.com>
2018-11-08 09:42:34 +0900

    properties 파일 외부로, 설정맞춤

--- src/main/java/tk/copyNpaste/member/NaverLogin.java
+++ src/main/java/tk/copyNpaste/member/NaverLogin.java
@@ -6,13 +6,13 @@
 */
 package tk.copyNpaste.member;
 
-import static org.junit.Assert.assertNotNull;
-
 import java.io.IOException;
 import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -27,6 +27,7 @@ import com.github.scribejava.core.oauth.OAuth20Service;
 
 import tk.copyNpaste.vo.MemberVO;
 
+@Service
 public class NaverLogin {
 
     /* 인증 요청문을 구성하는 파라미터 */
@@ -34,16 +35,20 @@ public class NaverLogin {
     //response_type: 인증 과정에 대한 구분값. code로 값이 고정돼 있습니다.
     //redirect_uri: 네이버 로그인 인증의 결과를 전달받을 콜백 URL(URL 인코딩). 애플리케이션을 등록할 때 Callback URL에 설정한 정보입니다.
     //state: 애플리케이션이 생성한 상태 토큰
-    private final static String CLIENT_ID = "sQ8TcG4J0Rrpb1APr9i1";       //네이버API Client ID
-    private final static String CLIENT_SECRET = "9LPlU2d3d1";                      
-    private final static String REDIRECT_URI = "http://localhost:8090/copyNpaste/member/naverOauth.do";
-    private final static String SESSION_STATE = "oauth_state";
-    /* 프로필 조회 API URL */
-    private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";/// Api 종류 기본 !!
+	//PROFILE_API_URL:  프로필 조회 API URL 
+
+	@Value("#{apikeys['naver.clientId']}")
+	private String CLIENT_ID; //네이버API Client ID
+	@Value("#{apikeys['naver.clientSecret']}")
+    private String CLIENT_SECRET; 
+	@Value("#{apikeys['naver.redirect_uri']}")
+    private String REDIRECT_URI;
+    private String SESSION_STATE = "oauth_state";
+    private String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
     
     /* 네이버 아이디로 인증  URL 생성  Method */
-    public static String getAuthorizationUrl(HttpSession session) {
-
+    public String getAuthorizationUrl(HttpSession session) {
+    	System.out.println(CLIENT_ID);
         /* 세션 유효성 검증을 위하여 난수를 생성 */
         String state = generateRandomString();
         /* 생성한 난수 값을 session에 저장 */
@@ -61,7 +66,7 @@ public class NaverLogin {
     }
 
     /* 네이버아이디로 Callback 처리 및  AccessToken 획득 Method */
-    public static OAuth2AccessToken getAccessToken(HttpSession session, String code, String state) throws IOException{
+    public OAuth2AccessToken getAccessToken(HttpSession session, String code, String state) throws IOException{
 
         /* Callback으로 전달받은 세선검증용 난수값과 세션에 저장되어있는 값이 일치하는지 확인 */
         String sessionState = getSession(session);
@@ -82,21 +87,21 @@ public class NaverLogin {
     }
 
     /* 세션 유효성 검증을 위한 난수 생성기 */
-    private static String generateRandomString() {
+    private String generateRandomString() {
         return UUID.randomUUID().toString();
     }
 
     /* http session에 데이터 저장 */
-    private static void setSession(HttpSession session,String state){
+    private void setSession(HttpSession session,String state){
         session.setAttribute(SESSION_STATE, state);     
     }
 
     /* http session에서 데이터 가져오기 */ 
-    private static String getSession(HttpSession session){
+    private String getSession(HttpSession session){
         return (String) session.getAttribute(SESSION_STATE);
     }
     /* Access Token을 이용하여 네이버 사용자 프로필 API를 호출 */
-    public static String getUserProfile(OAuth2AccessToken oauthToken) throws IOException{
+    public String getUserProfile(OAuth2AccessToken oauthToken) throws IOException{
 
         OAuth20Service oauthService =new ServiceBuilder()
                 .apiKey(CLIENT_ID)
@@ -109,7 +114,7 @@ public class NaverLogin {
         return response.getBody();
     }
     
-    public static MemberVO changeData(String profile) throws JsonProcessingException, IOException {
+    public MemberVO changeData(String profile) throws JsonProcessingException, IOException {
     	MemberVO member = new MemberVO();
 		// JSON 형태 반환값 처리
 		ObjectMapper mapper = new ObjectMapper();
